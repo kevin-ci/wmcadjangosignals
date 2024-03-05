ae2045fe2cca7d985ec5fc260a757610c94f8358
kevin-ci <kevin@codeinstitute.net>
2024-03-05 14:25:42 +0000

    final code

---
+++ profiles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    biography = models.TextField()
+    # profile_image = CloudinaryField('image', default='placeholder')
+    username = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.username
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, username=instance.username)
